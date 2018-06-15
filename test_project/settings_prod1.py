b83ff7c8e0144ed520394208855994ad1f2ae438
gordey0511 <39362878+gordey0511@users.noreply.github.com>
2018-06-15 18:53:56 +0600

    Update settings_prod1.py

--- test_project/settings_prod1.py
+++ test_project/settings_prod1.py
@@ -7,7 +7,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'db1',
         'USER': 'django_shop',
-        'PASSWORD': 'django_shop_test',
+        'PASSWORD': 'gordey04',
         'HOST': 'localhost',
         'PORT': '',                      # Set to empty string for default.
     }
